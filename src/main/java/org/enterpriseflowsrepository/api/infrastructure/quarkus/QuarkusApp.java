89ff867e911536387ccd874c9c0e6df71ef1fed7
Emmanuel <emmanuel.lesne@middleware-solutions.fr>
2019-10-29 18:57:38 +0100

    1st deployment GKE.

---
+++ src/main/java/org/enterpriseflowsrepository/api/infrastructure/quarkus/QuarkusApp.java
@@ -0,0 +1,8 @@
+package org.enterpriseflowsrepository.api.infrastructure.quarkus;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class QuarkusApp extends Application {
+
+}
