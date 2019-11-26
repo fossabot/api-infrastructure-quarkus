7b3a7dbea5bcb58371017ecc94f0f3c2e5c73543
Clément Lesné <clement@lesne.pro>
2019-11-26 14:39:24 +0100

    Init: CI/CD See: https://github.com/EnterpriseFlowsRepository/api-transcodifications-quarkus/pull/10/

--- deploy.cmd
+++
@@ -1,7 +0,0 @@
-rem & minikube docker-env | Invoke-Expression
-
-call mvn package
-
-call docker build -f src/main/docker/Dockerfile.jvm -t enterprise-flows-repository/api-infrastructure-quarkus:jvm .
-
-call kubectl apply -f src/main/k8s
--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mvn clean package
-& minikube docker-env | Invoke-Expression
-docker build -f src/main/docker/Dockerfile.jvm -t enterprise-flows-repository/api-properties-quarkus:jvm .
-kubectl apply -f src/main/k8s
