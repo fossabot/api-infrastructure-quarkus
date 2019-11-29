e3dd604a9b02387cf3ccf9d5f036cfc1ad7a5787
Clément Lesné <clement@lesne.pro>
2019-11-29 09:16:24 +0100

    Tests: Update ZAP step tests to improve its detection performance

--- container-test-zap.sh
+++ container-test-zap.sh
@@ -14,9 +14,15 @@ docker run -d $CONTAINER_NAME
 
 echo -e "\n\033[1m🔥 Trying to kill the beast...\033[0m"
 
-# Start attacking the ap
+# Attacking the app given the OpenAPI definitions
+# See: https://github.com/zaproxy/zaproxy/wiki/ZAP-API-Scan
+docker run -t owasp/zap2docker-weekly zap-api-scan.py -a -f openapi -t http://172.17.0.2:8080/openapi -O 172.17.0.2:8080
+
+echo -e "\n\033[1m🚒 Oh? Maybe with a little more effort...\033[0m"
+
+# Attacking the app with active scanning (time & CPU consuming)
 # See: https://github.com/zaproxy/zaproxy/wiki/ZAP-Baseline-Scan
-docker run -t owasp/zap2docker-stable zap-baseline.py -I -a -t http://172.17.0.2:8080/
+docker run -t owasp/zap2docker-weekly zap-full-scan.py -a -t http://172.17.0.2:8080/
 
 echo -e "\n\033[1m🚮 Picking up everything...\033[0m"
 
